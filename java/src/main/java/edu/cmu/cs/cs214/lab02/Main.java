bd9b0b93bc58845f061edf1dcb8e6343dbff775e
corincewang <hanwang3@andrew.cmu.edu>
2024-09-06 12:21:22 -0400

    add shapes and make renderer work by using shapes instead of specific shape types

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,19 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape square = new Square(2);
 
         Renderer renderer = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(square);
 
         renderer.draw();
+        renderer2.draw();
     }
 }
+
--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Shape.java
@@ -1,5 +1,16 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
+/**
+ * An interface representing a general geometric shape.
+ * Provides a contract for operations that all shapes must support.
+ */
 public interface Shape {
-    
+
+    /**
+     * Gets the area of the shape.
+     *
+     * @return the area of the shape as a double.
+     */
+    double getArea();
+
 }
